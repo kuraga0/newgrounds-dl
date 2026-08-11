c36b47be994722f7c7b25b096ba93d8ea05dfc92
Andrew <gus123tractor@gmail.com>
2026-08-11 17:11:17 +0400

    improve title converter

--- src/main.rs
+++ src/main.rs
@@ -15,21 +15,42 @@ struct Args {
 	open_in_browser: bool,
 }
 
-fn convert_name(name: String) -> String {
-	let mut result = name.clone();
+fn convert_title(title: String) -> String {
+	let entities: &[(char, &str)] = &[
+		('&', "amp;"),
+		('@', "at;"),
+		('/', "-"),
+		(' ', "-"),
+		//
+	];
 
-	result = result.replace(" ", "-");
-	result = result.chars().filter(|c| c.is_ascii_alphabetic()).collect();
+	let mut out = String::with_capacity(title.len());
+	'chars: for c in title.chars() {
+		if c == ' ' {
+			out.push('-');
+			continue;
+		}
+		for &(ch, name) in entities {
+			if c == ch {
+				out.push_str(name);
+				continue 'chars;
+			}
+		}
+		out.push(c);
+	}
 
-	result
+	out
 }
 
 fn convert_url(url: String, name: String) -> String {
-  let pos = url.rfind('/').unwrap() + 1;
+	let pos = url.rfind('/').unwrap() + 1;
 	let num: u64 = url[pos..].parse().unwrap();
 	let rounded = (num / 1000) * 1000;
 
-	format!("https://audio.ngfiles.com/{rounded}/{num}_{}.mp3", convert_name(name))
+	format!(
+		"https://audio.ngfiles.com/{rounded}/{num}_{}.mp3",
+		convert_title(name)
+	)
 }
 
 fn main() {
